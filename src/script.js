4ff1c5c351b779137859ff93640762a9fc40465e
RommJames <rj.cuya010@gmail.com>
2024-10-11 09:30:53 +0800

    Working Operators Buttons and Computation
    Some bug fixes t be fixed:
    - Need to fix display output
    - Need to fix Combination of Operators
    
    NOTE: Delete Button is not yet working and Dot button

--- src/script.js
+++ src/script.js
@@ -1,18 +1,24 @@
 // DOM
 const btnsCalculatorHTML = document.getElementById("btns-calculator");
-const displayOutputHTML = document.getElementById("display-output");
-const displayInputHTML = document.getElementById("display-input");
+const displayOutputHTML = document.querySelector("#display-output > h4");
+const displayInputHTML = document.querySelector("#display-input > h1");
 
 // Primitive Data
+let NumberCompiler = [];
+let recentOutput = [];
+let firstOperand;
+let secondOperand; 
+let activeOperator;
+let totalComputation;
 // +, -, *, /, AC, Del, ., =
 // keypads
 const keypads = 
 [
-    ["AC", "="],
-    [1, 2, 3, "+"],
-    [4, 5, 6, "-"],
-    [7, 8, 9, "*"],
-    ["Del", 0, ".", "/"]
+    ["AC", " = "],
+    [1, 2, 3, " + "],
+    [4, 5, 6, " - "],
+    [7, 8, 9, " * "],
+    ["Del", 0, " . ", " / "]
 ];
 
 keypads.map((row)=>{
@@ -21,7 +27,7 @@ keypads.map((row)=>{
     btnsCalculatorHTML.append(btnRows);
 
     row.map((btnValue)=>{
-        const btn = document.createElement("div");
+        const btn = document.createElement("button");
         btn.setAttribute("class", "btn"); 
         if(!isInt(btnValue)){
             btn.classList.add("btn-colored")   ;
@@ -81,9 +87,91 @@ function isInt(num){
     return Number.isInteger(num);
 }
 
+// clear all
+function AC(){
+    firstOperand = null;
+    secondOperand = null;
+    recentOutput = []
+    displayInputHTML.textContent = 0;  
+    displayOutputHTML.textContent = "";
+    totalComputation = 0;
+}
+
 // key functions
 function clickBtn(btnValue){
-    console.log(btnValue);
+
+    displayInput(btnValue);
+    
+}
+
+// Update Display Input
+
+function displayInput(btnValue){
+   
+
+    if(isInt(btnValue)){
+        NumberCompiler.push(btnValue);
+        recentOutput.push(btnValue);
+        displayInputHTML.textContent = NumberCompiler.join("");        
+
+    }else{
+        
+        recentOutput.push(btnValue)
+        
+        if(!firstOperand){
+            firstOperand = +(NumberCompiler.join(""));
+        }else{
+            secondOperand = +(NumberCompiler.join(""));
+        }
+        
+        
+
+        NumberCompiler = [];
+        displayInputHTML.textContent = "0"
+        console.log(firstOperand)
+
+        clickOperator(btnValue, firstOperand, secondOperand);
+    }
+    console.log(NumberCompiler)
+    console.log("first: ",firstOperand);
+    console.log("second: ",secondOperand);
+    displayOutputHTML.textContent = recentOutput.join("");
+}
+
+// Computation Functions
+
+function clickOperator(op, num1, num2){
+
+    op == "AC" && AC();
+    activeOperator = op != " = " ? op : activeOperator;
+
+    if(activeOperator && num1 && num2){
+        op == " = " ? operations(" = ", num1, num2) : operations(activeOperator, num1, num2);
+    }
+
+}
+
+function operations(option, num1, num2){
+    if(option == " = "){
+        computation(activeOperator, num1, num2);
+        alert(activeOperator)
+    }else{
+        computation(option, num1, num2);
+    }
+
+}
+
+function computation(op, num1, num2){
+    const operators = {
+        " + ": add(num1, num2),
+        " - ": subtract(num1, num2),
+        " * ": multiply(num1, num2),
+        " / ": divide(num1, num2),
+    }
+    console.log("total: ", operators[op]);
+    totalComputation = operators[op];
+    firstOperand = totalComputation;
+    displayInputHTML.textContent = totalComputation;
 }
 
 
